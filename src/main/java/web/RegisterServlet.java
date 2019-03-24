73cffc4afe0d832285eeaee47d31f41aaab73446
Norbert Daniluk <norbert.daniluk@gmail.com>
2019-03-24 18:58:33 +0100

    servlets initiated, jsp files moved, index.jsp created

---
+++ src/main/java/web/RegisterServlet.java
@@ -0,0 +1,12 @@
+package web;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RegisterServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response){
+
+    }
+}
+
---
+++ src/main/java/web/UsersListServlet.java
@@ -0,0 +1,11 @@
+package web;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UsersListServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response){
+
+    }
+}
